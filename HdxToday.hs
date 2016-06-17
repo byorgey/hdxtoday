a88cb50881387bd8739230839d93693da04775de
Brent Yorgey <byorgey@gmail.com>
2016-06-17 16:53:30 -0500

    great progress!  can now read & extract items

--- HdxToday.hs
+++ HdxToday.hs
@@ -1,27 +1,46 @@
+{-# LANGUAGE LambdaCase #-}
+
 -- from 'sandi'
 import qualified Codec.Binary.QuotedPrintable as QP
-import Text.HTML.TagSoup
+import           Text.HTML.TagSoup
+import           Text.StringLike
 
 import qualified Data.ByteString              as BS
+import           Data.List                    (intercalate)
+
+import           System.Environment
+
+data Item = Item { itemTitle :: String, itemDescription :: String }
+  deriving Show
+
+mkItem :: StringLike str => [str] -> Item
+mkItem (title:rest) =
+  Item (toString title) (concat . map toString $ rest)
+
+getItems :: FilePath -> IO [Item]
+getItems fn = do
+  raw <- BS.readFile fn
+  case QP.decode raw of
+    Left (parsed, leftovers) -> error "can't parse"
+    Right parsed -> do
+      let tags = parseTags parsed
+          items = map mkItem
+                . (map . map) fromTagText
+                . filter ((>1) . length)
+                . takeWhile ((~/= TagText "MENU") . head)
+                . map (filter (~/= TagText "\160"))
+                . map (filter (~/= TagText "\r\n"))
+                . map (filter (~== TagText ""))
+                . filter ((~/= TagText "|") . (!!1))
+                . partitions (~== TagOpen "b" [])
+                $ tags
+      return items
 
 main = do
   args <- getArgs
   case args of
     []     -> error "no file name"  -- read from stdin
     (fn:_) -> do
-      raw <- BS.readFile fn
-
-      case QP.decode raw of
-        Left (parsed, leftovers) -> error "can't parse"
-        Right parsed -> do
-          let tags = parseTags parsed
-              ps   = partitions (~== TagOpen "b" []) tags
-
-              -- throw away any <b> tag immediately followed by | char
-
-              -- After that, <b> tags will be either titles of sections
-              --   or titles of entries
-
-              -- After each entry title look for next <p> (throw it
-              -- away) and then text inside the <p> after that.
+      items <- getItems fn
+      putStr . unlines . map itemTitle $ items
 
